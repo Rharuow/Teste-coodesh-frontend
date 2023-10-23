35421eef37a32b6064c0255c0770da461db88986
Harysson <haryssonsoares@gmail.com>
2023-10-23 16:12:56 -0300

    feat: card component to list launch

--- src/app/components/Launches/List.tsx
+++ src/app/components/Launches/List.tsx
@@ -31,7 +31,15 @@ import {
 } from "@/components/ui/select";
 import { FilterParams } from "@/service/resources/launches";
 import Link from "next/link";
-import { VideoOff, Youtube } from "lucide-react";
+import {
+  ChevronLeft,
+  ChevronRight,
+  RocketIcon,
+  VideoOff,
+  Youtube,
+} from "lucide-react";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
 
 const headerTableItems = [
   "Id",
@@ -66,59 +74,71 @@ export const List = () => {
     data && createPagesList({ page, totalPages: data.totalPages });
 
   return (
-    <div className="flex w-full flex-wrap justify-center gap-3 rounded bg-accent p-3">
+    <div className="flex w-full flex-wrap items-center justify-between gap-3 rounded bg-accent p-3">
       <Text className="text-right text-sm text-slate-400">
         Registros de lançamentos
       </Text>
+      <div className="flex text-xs md:text-sm">
+        <Text className="hidden text-muted-foreground md:inline">
+          {data?.totalDocs} Lançament
+          {data && data.totalDocs && data.totalDocs > 1 ? "os" : "o"}
+        </Text>
+        <Text className="text-muted-foreground md:hidden">
+          {data?.totalDocs} Total
+        </Text>
+      </div>
 
       <div className="w-full">
-        <div className="flex items-center gap-3 py-4">
+        <div className="flex flex-col items-center gap-3 py-4 md:flex-row">
           <input type="hidden" {...register("results")} />
           <input type="hidden" {...register("limit")} />
           <Input
+            className="grow"
             placeholder="Nome da missão ou do foguete"
             {...register("search", { onChange: () => setPage(1) })}
           />
-          <Select
-            onValueChange={(value) => {
-              value !== "all"
-                ? setValue("results", value as FilterParams["results"])
-                : setValue("results", undefined);
-              setPage(1);
-            }}
-          >
-            <SelectTrigger className="max-w-[130px]">
-              <SelectValue placeholder="Resultados" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="all">Todos</SelectItem>
-              <SelectItem value="success">Sucesso</SelectItem>
-              <SelectItem value="fail">Falha</SelectItem>
-            </SelectContent>
-          </Select>
+          <div className="flex w-full gap-3">
+            <Select
+              onValueChange={(value) => {
+                value !== "all"
+                  ? setValue("results", value as FilterParams["results"])
+                  : setValue("results", undefined);
+                setPage(1);
+              }}
+            >
+              <SelectTrigger>
+                <SelectValue placeholder="Resultados" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="all">Todos</SelectItem>
+                <SelectItem value="success">Sucesso</SelectItem>
+                <SelectItem value="fail">Falha</SelectItem>
+              </SelectContent>
+            </Select>
 
-          <Select
-            onValueChange={(value) => {
-              setLimit(Number(value));
-              setPage(1);
-            }}
-          >
-            <SelectTrigger className="max-w-[80px]">
-              <SelectValue placeholder={limit} />
-            </SelectTrigger>
-            <SelectContent>
-              {Array(10)
-                .fill(null)
-                .map((_, index) => (
-                  <SelectItem key={index} value={String((index + 1) * 5)}>
-                    {(index + 1) * 5}
-                  </SelectItem>
-                ))}
-            </SelectContent>
-          </Select>
+            <Select
+              onValueChange={(value) => {
+                setLimit(Number(value));
+                setPage(1);
+              }}
+            >
+              <SelectTrigger className="max-w-[80px]">
+                <SelectValue placeholder={limit} />
+              </SelectTrigger>
+              <SelectContent>
+                {Array(10)
+                  .fill(null)
+                  .map((_, index) => (
+                    <SelectItem key={index} value={String((index + 1) * 5)}>
+                      {(index + 1) * 5}
+                    </SelectItem>
+                  ))}
+              </SelectContent>
+            </Select>
+          </div>
         </div>
         <div className="rounded-md border">
-          <Table>
+          <Table className="hidden md:table">
             <TableHeader>
               <TableRow>
                 {headerTableItems.map((item, index) => (
@@ -138,7 +158,9 @@ export const List = () => {
               ) : data?.results.length ? (
                 data.results.map((launch, index) => (
                   <TableRow key={launch.id}>
-                    <TableCell>{index}</TableCell>
+                    <TableCell>
+                      <Text>{index}</Text>
+                    </TableCell>
                     <TableCell>
                       <Image
                         src={
@@ -150,11 +172,17 @@ export const List = () => {
                         height={30}
                       />
                     </TableCell>
-                    <TableCell>{launch.name}</TableCell>
                     <TableCell>
-                      {dayjs(launch.date_local).format("DD/MM/YYYY")}
+                      <Text>{launch.name}</Text>
                     </TableCell>
-                    <TableCell>{launch.rocket.name}</TableCell>
+                    <TableCell>
+                      <Text>
+                        {dayjs(launch.date_local).format("DD/MM/YYYY")}
+                      </Text>
+                    </TableCell>
+                    <TableCell>
+                      <Text>{launch.rocket.name}</Text>
+                    </TableCell>
                     <TableCell>
                       <Text
                         className={
@@ -167,7 +195,7 @@ export const List = () => {
                     <TableCell>
                       {launch.links.webcast ? (
                         <Link target="_blank" href={launch.links.webcast}>
-                          <Youtube />
+                          <Youtube className="text-red-500" />
                         </Link>
                       ) : (
                         <VideoOff />
@@ -196,18 +224,78 @@ export const List = () => {
               )}
             </TableBody>
           </Table>
+          <div className="flex flex-col gap-3 md:hidden">
+            {data?.results.map((launch, index) => (
+              <Card key={index}>
+                <div className="flex gap-3 p-3">
+                  <div className="flex">
+                    <Text>{index}</Text>
+                  </div>
+                  <div className="flex flex-1 items-start justify-center">
+                    <Text className="text-center">{launch.name}</Text>
+                  </div>
+                  <div className="flex items-start justify-center">
+                    {launch.links.patch.small ? (
+                      <Image
+                        src={launch.links.patch.small}
+                        alt="logo mission"
+                        width={0}
+                        height={0}
+                        sizes="100vw"
+                        className="w-[15px]"
+                      />
+                    ) : (
+                      <Text>
+                        <RocketIcon className="w-[15px]" />
+                      </Text>
+                    )}
+                  </div>
+                </div>
+                <Separator />
+                <CardContent>
+                  <div className="flex flex-col gap-2 py-2">
+                    <div className="flex justify-between">
+                      <Text className="text-xs">Foguete:</Text>
+                      <Text className="text-xs">{launch.rocket.name}</Text>
+                    </div>
+                    <div className="flex justify-between">
+                      <Text className="text-xs">Data:</Text>
+                      <Text className="text-xs">
+                        {dayjs(launch.date_utc).format("DD/MM/YYYY")}
+                      </Text>
+                    </div>
+                    <div className="flex justify-between">
+                      <Text className="text-xs">Sucesso:</Text>
+                      <Text
+                        className={`text-xs ${
+                          launch.success ? "text-green-500" : "text-red-500"
+                        }`}
+                      >
+                        {launch.success ? "sucesso" : "Falha"}
+                      </Text>
+                    </div>
+                    <div className="flex justify-between">
+                      <Text className="text-xs">Vídeo:</Text>
+                      {launch.links.webcast ? (
+                        <Link target="_blank" href={launch.links.webcast}>
+                          <Youtube className="text-red-500" />
+                        </Link>
+                      ) : (
+                        <VideoOff />
+                      )}
+                    </div>
+                  </div>
+                </CardContent>
+              </Card>
+            ))}
+          </div>
         </div>
         <div className="flex items-center justify-end space-x-2 py-4">
-          <div className="text-sm text-muted-foreground">
-            <Text className="text-slate">
-              {data?.totalDocs} Lançament
-              {data && data.totalDocs && data.totalDocs > 1 ? "os" : "o"}
-            </Text>
-          </div>
-          <div className="flex flex-1 justify-center gap-2 overflow-hidden">
+          <div className="scrollbar-hide flex  flex-1 gap-2 overflow-x-scroll">
             {listPages?.map((pg, index) => (
               <Button
                 key={index}
+                size={"sm"}
                 className={`rounded-full ${
                   pg === page ? "bg-slate-300" : "bg-slate-600 text-white"
                 }`}
@@ -224,20 +312,22 @@ export const List = () => {
               </Button>
             ))}
           </div>
-          <div className="space-x-2">
+          <div className="flex space-x-2 ">
             <Button
               variant="outline"
-              size="sm"
+              size="icon"
+              className="rounded-full"
               onClick={() => {
                 setPage((prev) => (prev === 1 ? 0 : prev - 1));
               }}
               disabled={!data?.hasPrev}
             >
-              Previous
+              <ChevronLeft />
             </Button>
             <Button
               variant="outline"
-              size="sm"
+              size="icon"
+              className="rounded-full"
               onClick={() => {
                 setPage((prev) =>
                   prev < Number(data?.totalPages)
@@ -247,7 +337,7 @@ export const List = () => {
               }}
               disabled={!data?.hasNext}
             >
-              Next
+              <ChevronRight />
             </Button>
           </div>
         </div>
