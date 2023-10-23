50e2fb27a6f9884cd43bb2f85ce168546c22a2ad
Harysson <haryssonsoares@gmail.com>
2023-10-23 08:27:38 -0300

    feat: add register to input filter list launches

--- src/app/components/Launches/List.tsx
+++ src/app/components/Launches/List.tsx
@@ -2,8 +2,11 @@
 import { Text } from "@/components/Text";
 import { Input } from "@/components/ui/input";
 import React from "react";
+import { useForm } from "react-hook-form";
 
 export const List = () => {
+  const { register } = useForm();
+
   return (
     <div className="flex w-full flex-wrap justify-center gap-3 rounded bg-accent p-3">
       <Text className="text-right text-sm text-slate-400">
@@ -11,7 +14,10 @@ export const List = () => {
       </Text>
 
       <div className="flex">
-        <Input placeholder="Nome da missão ou do foguete" />
+        <Input
+          placeholder="Nome da missão ou do foguete"
+          {...register("search")}
+        />
       </div>
     </div>
   );
